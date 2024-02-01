13c5f8f27b183e73e853c13c3462e7e3e3ecc096
MariamAbdeen <mariamabdeen299@gmail.com>
2024-02-01 16:38:05 +0200

    Patient Entity and Its Configuration

---
+++ HospitalityModule/Data/Configuration/PatientConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using HospitalityModules.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace HospitalityModules.Data.Configuration
+{
+    public class PatientConfiguration : IEntityTypeConfiguration<Patient>
+    {
+        public void Configure(EntityTypeBuilder<Patient> builder)
+        {
+            builder.Property(p => p.Name).IsRequired();
+            builder.Property(p => p.Gender).IsRequired();
+
+
+
+        }
+    }
+}
---
+++ HospitalityModule/Entities/Patient.cs
@@ -0,0 +1,16 @@
+﻿using HospitalityModule.Entities;
+
+namespace HospitalityModules.Entities
+{
+    public class Patient : BaseEntity
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Gender { get; set; }
+        public ICollection<RoomServicesRequest> ServiceRequests { get; set; }
+        public ICollection<Visitor> Visitors { get; set; } = new HashSet<Visitor>();
+
+
+    }
+}
