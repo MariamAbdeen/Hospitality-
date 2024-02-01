32a51c804c73c40758005d29cff639db757cf340
MariamAbdeen <mariamabdeen299@gmail.com>
2024-02-01 17:29:50 +0200

    Add Visitor Configuration

---
+++ HospitalityModule/Data/Configuration/VisitorConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using HospitalityModules.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace HospitalityModules.Data.Configuration
+{
+    public class VisitorConfiguration : IEntityTypeConfiguration<Visitor>
+    {
+        public void Configure(EntityTypeBuilder<Visitor> builder)
+        {
+            builder.Property(v => v.Name).IsRequired();
+        }
+    }
+}
