c595573770220234d97b35d32b2e2c5b3501e188
MariamAbdeen <mariamabdeen299@gmail.com>
2024-02-01 17:01:17 +0200

    Add Room Dto

---
+++ HospitalityModule/Dtos/RoomDto.cs
@@ -0,0 +1,14 @@
+﻿namespace HospitalityModules.DTOs
+{
+    public class RoomDto
+    {
+
+        public int Number { get; set; }
+        public string Type { get; set; }
+        public bool Availability { get; set; } = true;
+        public string Status { get; set; }
+        // public ICollection<ServicesRequestDto> ServiceRequests { get; set; }
+
+
+    }
+}
