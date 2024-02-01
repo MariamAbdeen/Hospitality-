cb28987f0dc22ef8e40ef2ce93f3fb3f3f870910
MariamAbdeen <mariamabdeen299@gmail.com>
2024-02-01 17:25:52 +0200

    Add Room services Request Dto

---
+++ HospitalityModule/Dtos/ServicesRequestDto.cs
@@ -0,0 +1,11 @@
+﻿namespace HospitalityModules.DTOs
+{
+    public class ServicesRequestDto
+    {
+        public int RoomId { get; set; }
+        public string ServicesType { get; set; }
+        public string Status { get; set; }
+
+    }
+}
+
