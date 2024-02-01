484d6a0c67b760e5afcfab2a00373f1429360f2e
MariamAbdeen <mariamabdeen299@gmail.com>
2024-02-01 17:25:03 +0200

    Room services Request configuration

---
+++ HospitalityModule/Data/Configuration/RoomServicesRequestConfig.cs
@@ -0,0 +1,23 @@
+﻿using HospitalityModules.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace HospitalityModules.Data.Configuration
+{
+    public class RoomServicesRequestConfig : IEntityTypeConfiguration<RoomServicesRequest>
+    {
+        public void Configure(EntityTypeBuilder<RoomServicesRequest> builder)
+        {
+            builder.Property(S => S.ServicesType).IsRequired();
+            builder.Property(S => S.Status).IsRequired();
+            //builder.HasOne(S => S.Room)
+            //       .WithMany()
+            //       .HasForeignKey(S => S.RoomId);
+            //builder.HasOne(S=>S.Patient)
+            //       .WithMany()
+            //       .HasForeignKey (S=>S.PatientId);
+
+
+        }
+    }
+}
