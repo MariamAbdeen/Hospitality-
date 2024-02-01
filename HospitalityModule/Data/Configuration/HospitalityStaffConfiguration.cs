ae64a19b4d8b4cb0abed869601bbe22a818d1c0d
MariamAbdeen <mariamabdeen299@gmail.com>
2024-02-01 17:06:00 +0200

    Add Hospitality Staff Configuration

---
+++ HospitalityModule/Data/Configuration/HospitalityStaffConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using HospitalityModules.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace HospitalityModules.Data.Configuration
+{
+    public class HospitalityStaffConfiguration : IEntityTypeConfiguration<HospitalityStaff>
+    {
+        public void Configure(EntityTypeBuilder<HospitalityStaff> builder)
+        {
+            builder.Property(x => x.Name).IsRequired();
+            builder.Property(x => x.Role).IsRequired();
+        }
+    }
+}
