1c991cddeceaccfa7b657597105c558c4806aebe
MariamAbdeen <mariamabdeen299@gmail.com>
2024-02-01 16:56:17 +0200

    Add Room Configuration

---
+++ HospitalityModule/Data/Configuration/RoomConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using HospitalityModules.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace HospitalityModules.Data.Configuration
+{
+    public class RoomConfiguration : IEntityTypeConfiguration<Room>
+    {
+        public void Configure(EntityTypeBuilder<Room> builder)
+        {
+            builder.Property(R => R.Type).IsRequired();
+        }
+    }
+}
