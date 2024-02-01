a94d249552c6e67468168d486af9ea02773a0d1c
MariamAbdeen <mariamabdeen299@gmail.com>
2024-02-01 16:55:23 +0200

    Add Room  Entities

---
+++ HospitalityModule/Entities/Room.cs
@@ -0,0 +1,19 @@
+﻿
+using HospitalityModule.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace HospitalityModules.Entities
+{
+    public class Room : BaseEntity
+    {
+        public int Number { get; set; }
+        public string Type { get; set; }
+        public bool Availability { get; set; } = true;
+        public int? PatientId { get; set; }
+        //public Patient Patient { get; set; }
+
+        //Navigation property (Many)
+        public ICollection<RoomServicesRequest> ServiceRequests { get; set; } = new HashSet<RoomServicesRequest>();
+
+    }
+}
