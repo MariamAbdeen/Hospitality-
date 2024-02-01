c5e479839b319010730972d2563ddb16ada25ab3
MariamAbdeen <mariamabdeen299@gmail.com>
2024-02-01 16:48:16 +0200

    Add Patient Dto

---
+++ HospitalityModule/Dtos/PatientDto.cs
@@ -0,0 +1,15 @@
+﻿using HospitalityModules.Entities;
+
+namespace HospitalityModules.DTOs
+{
+    public class PatientDto
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Gender { get; set; }
+
+        //public ICollection <ServicesRequestDto>  ServiceRequests { get; set; }
+        //public ICollection< VisitorsDto > Visitors{ get; set; } 
+    }
+}
