1f167c09e6a6a2f91646bb6b45bc1b8296067dd6
MariamAbdeen <mariamabdeen299@gmail.com>
2024-02-01 17:28:32 +0200

    Add Visitor Entities

---
+++ HospitalityModule/Entities/Visitor.cs
@@ -0,0 +1,12 @@
+﻿using HospitalityModule.Entities;
+
+namespace HospitalityModules.Entities
+{
+    public class Visitor : BaseEntity
+    {
+        public string Name { get; set; }
+        public int PatientId { get; set; }
+        /*public Patient Patient { get; set; }*/ //Navigation Property(ONE)
+
+    }
+}
